3aceaa26470e9487316ed95047b60cb2c46d3897
Andrew-Schwartz <andrewschwartz59@gmail.com>
2023-04-17 20:45:47 -0400

    constants

--- YouTilities.m
+++ YouTilities.m
@@ -11,3 +11,12 @@ sideEffect[f_][x_]:=(f@x;x)
 
 
 print[f_][x_]:=sideEffect[Print@*f][x]
+
+
+loadConstants[]:=(
+\[HBar]=Subscript["ReducedPlanckConsant", q];
+c=Subscript["SpeedOfLight", q];
+Subscript[q, e]=Subscript["ElectronCharge", q];
+Subscript[m, e]=Subscript["ElectronMass", q];
+Subscript[\[Epsilon], 0]=Subscript["VacuumPermittivity", q];
+)
