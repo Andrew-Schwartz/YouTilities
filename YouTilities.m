c5e900db125d5f706d843b09dc08b9d366014b13
Andrew-Schwartz <andrewschwartz59@gmail.com>
2023-09-13 14:48:29 -0400

    load sum, product, etc of constants

--- YouTilities.m
+++ YouTilities.m
@@ -16,14 +16,24 @@ alsoPrint[f_:Identity]:=also[Print@*f]
 printThen[f_][x_]:=Module[]
 
 
-loadQuantites[\[HBar]]:=\[HBar]=Quantity["ReducedPlanckConstant"];
-loadQuantites[c]:=c=Quantity["SpeedOfLight"];
-loadQuantites[Subscript[q, e]]:=Subscript[q, e]=Quantity["ElectronCharge"];
-loadQuantites[Subscript[m, e]]:=Subscript[m, e]=Quantity["ElectronMass"];
-loadQuantites[Subscript[\[Epsilon], 0]]:=Subscript[\[Epsilon], 0]=Quantity["VacuumPermittivity"];
-loadQuantites[Subscript[\[Mu], 0]]:=Subscript[\[Mu], 0]=Quantity["VacuumPermeability"];
+ClearAll@loadQuantites;
+(* Makes loadQuantities idempotent, so loadQuantities[loadQuantities[\[HBar]]]==loadQuantities[\[HBar]] *)
+loadQuantites[q_Quantity]:=q;
+(* Define constants that can be loaded *)
+loadQuantites[HoldPattern@\[HBar]]:=\[HBar]=Quantity["ReducedPlanckConstant"];
+loadQuantites[HoldPattern@c]:=c=Quantity["SpeedOfLight"];
+loadQuantites[HoldPattern@Subscript[q, e]]:=Subscript[q, e]=Quantity["ElectronCharge"];
+loadQuantites[HoldPattern@Subscript[m, e]]:=Subscript[m, e]=Quantity["ElectronMass"];
+loadQuantites[HoldPattern@Subscript[\[Epsilon], 0]]:=Subscript[\[Epsilon], 0]=Quantity["VacuumPermittivity"];
+loadQuantites[HoldPattern@Subscript[\[Mu], 0]]:=Subscript[\[Mu], 0]=Quantity["VacuumPermeability"];
+(* Allow list of quantities to be loaded  *)
 SetAttributes[loadQuantites,Listable]
-(*loadQuantites[symbs__]:=(loadQuantites@#)&/@{symbs}*)
+(* Handle other symbols not defined above, and return 0 *)
+loadQuantites::unknownSymb="Unknown symbol `1`";
+loadQuantites[symb_]:=(Message[loadQuantites::unknownSymb,symb];0);
+(* Load product, sum, etc of constants at once *)
+loadQuantites[HoldPattern[f_[symbs__]]]:=f@@loadQuantites[{symbs}]
+(* Allow list of quantities to be loaded without surrounding in brackets *)
 loadQuantites[symbs__]:=loadQuantites[{symbs}]
 
 
